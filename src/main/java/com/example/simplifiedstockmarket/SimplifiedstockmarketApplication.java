7277fd72f3a0565931f85e720d3ca50fa4477946
ciastek <jezowdominik@duck.com>
2026-04-21 19:53:07 +0200

    springBoot init

---
+++ src/main/java/com/example/simplifiedstockmarket/SimplifiedstockmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplifiedstockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedstockmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedstockmarketApplication.class, args);
+	}
+
+}
